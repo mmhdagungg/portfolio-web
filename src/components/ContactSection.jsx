717450814335451c4fd335883b9756804750ae68
mmhdagungg <mmhdagung7@gmail.com>
2025-08-09 22:40:27 +0700

    solve link to contact

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -9,7 +9,7 @@ export const ContactSection = () => {
     //     }, 1500)
     // }
 
-    return <section id="contatc" className="py-24 px-4 relative bg-secondary/30">
+    return <section id="contact" className="py-24 px-4 relative bg-secondary/30">
         <div className="container mx-auto max-w-5xl">
             <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> Get In <span className="text-primary"> Touch</span></h2>
 
